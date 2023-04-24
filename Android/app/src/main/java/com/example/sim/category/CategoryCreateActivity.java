383866268ddb85e2965ad703a7bd6914d7a59312
novakvova <novakvova@gmail>
2023-04-24 18:53:23 +0300

    Working menu go to acrivity

---
+++ Android/app/src/main/java/com/example/sim/category/CategoryCreateActivity.java
@@ -0,0 +1,16 @@
+package com.example.sim.category;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sim.BaseActivity;
+import com.example.sim.R;
+
+public class CategoryCreateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_create);
+    }
+}
