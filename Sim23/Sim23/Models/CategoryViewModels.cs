dba3c69a24942af3a798bab2253b8e26a128ef5a
novakvova <novakvova@gmail>
2023-05-01 21:09:22 +0300

    Get by id all data

--- Sim23/Sim23/Models/CategoryViewModels.cs
+++ Sim23/Sim23/Models/CategoryViewModels.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Priority { get; set; }
+        public string Description { get; set; }
         public string Image { get; set; }
     }
 
