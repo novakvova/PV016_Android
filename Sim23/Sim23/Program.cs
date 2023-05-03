e68d15021f1cf8ed2f272325bb2d5309578f5f0a
novakvova <novakvova@gmail>
2023-05-03 20:22:03 +0300

    Allow Cors

--- Sim23/Sim23/Program.cs
+++ Sim23/Sim23/Program.cs
@@ -55,13 +55,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(options =>
+                options.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
+app.UseSwagger();
     app.UseSwaggerUI();
 //}
 
